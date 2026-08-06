66c27816b6bc28690b49a6e1933560c62004ff2d
anikroy4 <anikroy.uiu@gmail.com>
2026-08-06 19:07:20 +0600

    name validationand controller install;
    name validation and controller install;

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,10 @@
 const User = require('../models/userModel');
+const  {validateEmail,validateName, validatePassword, validateUsername}  = require('../helpers/validation');
 
+const bcrypt = require('bcrypt');   
 
 exports.newUser = async (req, res) => {
-    try
-    {
+    try {
         const {
             fName,
             lName,
@@ -14,25 +15,75 @@ exports.newUser = async (req, res) => {
             bYear,
             bDay,
             gender,
-            verified   
-            } = req.body;
+            verified
+        } = req.body;
 
-        const user= await new User({
-           fName,
+
+        if (!validateEmail(email)) {
+            return res.status(400).json({
+                message: "Please enter a valid email !"
+            });
+            
+        }
+        const checkMail= await User.findOne({email});
+
+        if(checkMail){
+            return res.status(400).json({
+                message: "This email already exists !"
+            });
+        }
+
+        if(!validateName(fName,4,15)){
+            return res.status(400).json
+            ({
+                message: "First name must be between 3 and 15 characters long !"
+            });
+        }
+        if(!validateName(lName,3,15)){
+            return res.status(400).json
+            ({
+                message: "Last name must be between 3 and 15 characters long !"
+            });
+        }
+
+        if(!validatePassword(password,8,40)){
+            return res.status(400).json
+            ({
+                message: "Password must be at least 8 characters long !"
+            });
+        }
+        //bycript password
+        const crypted = await bcrypt.hash(password, 12);
+
+    
+
+        console.log(crypted);
+        console.log('====================================');
+        //validate username
+        let tempUsername = fName + lName;
+
+        let finalUsername = await validateUsername((tempUsername).toLowerCase());
+
+
+
+        
+        const user = await new User({
+            fName,
             lName,
-            username,
+            username: finalUsername,
             email,
-            password,
+            password: crypted,
             bMonth,
             bYear,
             bDay,
             gender,
-            verified 
-            }).save();
+            verified
+        }).save();
+
         res.send(user);
-        
+
     }
-    catch(err){
+    catch (err) {
         res.status(404).json({
             message: "Error while creating user",
             error: err.message
---
+++ backend/helpers/validation.js
@@ -0,0 +1,41 @@
+const User = require('../models/userModel');
+
+exports.validateEmail =(email) => {
+ 
+    return String(email).toLowerCase()
+    .match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+)
+}
+
+exports.validateName =  (text,min,max) => {
+    if(text.length< min || text.length>max){
+        return false;
+    }else{
+        return true;
+    }
+}   
+
+exports.validatePassword= (password, min, max) => {
+    if(password.length<min || password.length>max){
+        return false;   
+    }else{
+        return true;
+    }   
+}
+
+exports.validateUsername = async (username) => {
+    let istrue = false;
+    do{
+        let user = await User.findOne({username});
+        if(user){
+            username += (+new Date()* Math.random()).toString().substring(0, 1);
+            istrue = true;  
+        }else{
+            istrue = false;
+        }
+
+    } while(istrue)
+    return username;
+
+}
