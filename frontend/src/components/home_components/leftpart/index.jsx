1267c185466c8272d1b7f58df927807901cdf107
anikroy4 <anikroy.uiu@gmail.com>
2026-08-27 00:16:48 +0600

    skeleton of hjome added

---
+++ frontend/src/components/home_components/leftpart/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LeftProfile from './LeftProfile'
+
+const LeftPart = () => {
+  return (
+    <>
+    <LeftProfile />
+    
+    </>
+  )
+}
+
+export default LeftPart
---
+++ frontend/src/components/home_components/postpart/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostPart = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default PostPart
